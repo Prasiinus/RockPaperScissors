df544490fa05afc9bc2b2833d61ed77701c6a3b5
Prasiinus <valentind9@hotmail.fr>
2022-11-04 20:29:17 +0100

    ajout de la fonction getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("coucou");
+const game = ['Rock', 'Paper', 'Scissors'];
+let computerChoice ='start';
+
+function getComputerChoice() {
+return computerChoice = game[Math.floor(Math.random()*game.length)];
+}
+
+console.log(computerChoice);
+getComputerChoice();
+console.log(computerChoice);
