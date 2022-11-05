40b5113a9b0933752507908209a0d98fbc9056ed
Prasiinus <valentind9@hotmail.fr>
2022-11-05 21:15:19 +0100

    ajout du score

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const game = ['pierre', 'feuille', 'ciseaux'];
-
+let monscore;
 
 function getComputerChoice() {
     return game[Math.floor(Math.random() * game.length)];
@@ -10,18 +10,21 @@ const computerChoice = getComputerChoice();
 const playerSelection = prompt('Your turn').toLowerCase();
 console.log('Choix de l\'ordinateur : ' + computerChoice);
 console.log('Mon choix : ' + playerSelection);
+
     switch (true) {
         case computerChoice === playerSelection:
             alert("Match null");
             break;
         case computerChoice === 'pierre' && playerSelection === 'feuille':
             alert('La feuille enveloppe la pierre, Gagné !!');
+            monscore++
             break;
         case computerChoice === 'feuille' && playerSelection === 'pierre':
             alert('La pierre est enveloppé par la feuille, Perdu !!');
             break;
         case computerChoice === 'ciseaux' && playerSelection === 'pierre':
             alert('La pierre écrase les ciseaux, Gagné !!');
+            monscore++
             break;
         case computerChoice === 'pierre' && playerSelection === 'ciseaux':
             alert('Les ciseaux sont écrasés par la pierre, Perdu !!');
@@ -31,6 +34,7 @@ console.log('Mon choix : ' + playerSelection);
             break;
         case computerChoice === 'feuille' && playerSelection === 'ciseaux':
             alert('Les ciseaux coupent la feuille, Gagné !!');
+            monscore++
             break;
 
         default: alert('Rien ne se passe . . . ');
@@ -39,11 +43,13 @@ console.log('Mon choix : ' + playerSelection);
 }
 
 
-
 function jeu()
 {
+    //ajouter le comptage de score ici 
+    monscore=0;
     for (let i = 0; i < 5; i++) {
        playRound();
+       console.log(monscore)
      }
     }
 jeu();
