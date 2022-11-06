227ba4d7250d87b1fae6f0e5c27a8e6048e0a5cf
Prasiinus <valentind9@hotmail.fr>
2022-11-06 13:09:01 +0100

    ajout du score

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const game = ['pierre', 'feuille', 'ciseaux'];
 let monscore;
+let ordiscore;
 
 function getComputerChoice() {
     return game[Math.floor(Math.random() * game.length)];
 }
 
 function playRound() {
-const computerChoice = getComputerChoice();
-const playerSelection = prompt('Your turn').toLowerCase();
-console.log('Choix de l\'ordinateur : ' + computerChoice);
-console.log('Mon choix : ' + playerSelection);
+    const computerChoice = getComputerChoice();
+    const playerSelection = prompt('Your turn').toLowerCase();
+    console.log('Choix de l\'ordinateur : ' + computerChoice);
+    console.log('Mon choix : ' + playerSelection);
 
     switch (true) {
         case computerChoice === playerSelection:
@@ -21,6 +22,7 @@ console.log('Mon choix : ' + playerSelection);
             break;
         case computerChoice === 'feuille' && playerSelection === 'pierre':
             alert('La pierre est enveloppé par la feuille, Perdu !!');
+            ordiscore++
             break;
         case computerChoice === 'ciseaux' && playerSelection === 'pierre':
             alert('La pierre écrase les ciseaux, Gagné !!');
@@ -28,9 +30,11 @@ console.log('Mon choix : ' + playerSelection);
             break;
         case computerChoice === 'pierre' && playerSelection === 'ciseaux':
             alert('Les ciseaux sont écrasés par la pierre, Perdu !!');
+            ordiscore++
             break;
         case computerChoice === 'ciseaux' && playerSelection === 'feuille':
             alert('La feuille est coupé par les ciseaux, Perdu !!');
+            ordiscore++
             break;
         case computerChoice === 'feuille' && playerSelection === 'ciseaux':
             alert('Les ciseaux coupent la feuille, Gagné !!');
@@ -43,13 +47,20 @@ console.log('Mon choix : ' + playerSelection);
 }
 
 
-function jeu()
-{
+function jeu() {
     //ajouter le comptage de score ici 
-    monscore=0;
+    monscore = 0;
+    ordiscore = 0;
     for (let i = 0; i < 5; i++) {
-       playRound();
-       console.log(monscore)
-     }
+        playRound();
+        console.log('Player score : ' + monscore)
+        console.log('Computer score : ' + ordiscore)
     }
+    if (ordiscore > monscore) {
+        alert('L\'ordinateur à gagné ! ' + ordiscore+'-'+monscore)
+    } else if (monscore > ordiscore) {
+        alert('Vous avez gagné ! ' + monscore +'-' +ordiscore)
+    } else  alert('Match nul ! ' +monscore+'-'+ordiscore)
+
+}
 jeu();
