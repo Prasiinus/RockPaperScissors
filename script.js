f567cf1b27e9a997da6b39866c297b65a55d6c75
Prasiinus <valentind9@hotmail.fr>
2023-02-15 16:14:15 +0100

    maj du projet

--- script.js
+++ script.js
@@ -63,3 +63,19 @@ function jeu() {
 
 }
 jeu();
+
+const results = document.querySelector('#results');
+
+function calculate() {
+  for (let i = 1; i < 10; i++) {
+    const newResult = `${i} x ${i} = ${i * i}`;
+    results.textContent += `${newResult}\n`;
+  }
+  results.textContent += '\nFinished!';
+}
+
+const calculateBtn = document.querySelector('#calculate');
+const clearBtn = document.querySelector('#clear');
+
+calculateBtn.addEventListener('click', calculate);
+clearBtn.addEventListener('click', () => results.textContent = '');
